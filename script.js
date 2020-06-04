c7e5b4fe329318f43275fe910e8c187ea3871dbc
Anton <toxajlb@yandex.ru>
2020-06-04 14:45:52 +0300

    added some attribute

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt('Один из просмотренных последних фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из просмотренных последних фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
